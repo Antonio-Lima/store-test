97e6c090b8a66b6e7d5db9d09b7ba6c32198a4e9
Antônio Lima <gutto1998@gmail.com>
2024-04-04 14:31:46 -0300

    Munu button implemented.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 import { MdOutlineStore } from "react-icons/md";
 
+import MenuBtn from "../MenuBtn";
+import { menuItems } from "@/constants";
+
 export default function Header() {
   return (
     <header className="bg-default bg-opacity-80 py-4 flex justify-center">
-      <nav className="max-w-5xl w-full px-5 flex gap-6 items-center">
+      <nav className="max-w-5xl w-full px-5 flex gap-6 items-center justify-between sm:justify-start">
         <Link
           href="/"
           className="text-gray-800 ease-in-out duration-300 hover:text-gray-400 text-2xl font-bold flex items-center gap-1"
@@ -13,14 +16,15 @@ export default function Header() {
           <MdOutlineStore size={48} />
           Store
         </Link>
-        <div className="flex gap-4 items-center">
+
+        <div className="gap-4 items-center hidden sm:flex">
           <Link
             className="text-gray-500 ease-in-out duration-300 hover:brightness-50 hover:text-lg"
             href="/products"
           >
             Produtos
           </Link>
-          {["Contato", "Sobre", "Dúvidas"].map((item, index) => (
+          {menuItems.map((item, index) => (
             <h2
               key={index}
               className="text-gray-500 ease-in-out duration-300 hover:brightness-50 hover:text-lg"
@@ -29,6 +33,8 @@ export default function Header() {
             </h2>
           ))}
         </div>
+
+        <MenuBtn />
       </nav>
     </header>
   );
